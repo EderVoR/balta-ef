102d32b139685247d6d2368ffd0a4f09d8845dc4
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2024-11-18 10:08:50 -0400

    Inicio de mapeamento fluent no projeto

--- FluentBlog/Data/BlogDataContext.cs
+++ FluentBlog/Data/BlogDataContext.cs
@@ -5,14 +5,6 @@ namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Post> Posts { get; set; }
-        // public DbSet<PostTag> PostTags { get; set; }
-        // public DbSet<UserRole> UserRoles { get; set; }
-        public DbSet<Usuario> Usuarios { get; set; }
-        // public DbSet<Grupo> Grupos { get; set; }
-        // public DbSet<Tag> Tags { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("User ID=postgres; Password=admin123; Server=localhost; Port=5432; Database=FluentBlog; Pooling=true;");
